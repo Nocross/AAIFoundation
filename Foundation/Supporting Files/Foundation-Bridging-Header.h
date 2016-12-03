d9bc64cdbe3ae7d8f2e83b04e593e1e8bf0f7203
Andrey Ilskiy <nocross@users.noreply.github.com>
2016-12-03 20:36:56 +0300

    imported Supporting Files

---
+++ Foundation/Supporting Files/Foundation-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Foundation-Bridging-Header.h.h
+//  Foundation
+//
+//  Created by Andrey Ilskiy on 03/12/2016.
+//  Copyright © 2016 Andrey Ilskiy. All rights reserved.
+//
+
+#ifndef Foundation_Bridging_Header_h_h
+#define Foundation_Bridging_Header_h_h
+
+#include "Version.h"
+
+#endif /* Foundation_Bridging_Header_h_h */
